17841cb823a8032c4408ebc5f60f4b86e73e9369
NotDaPoyo <168030791+NotDaPoyo@users.noreply.github.com>
2024-04-24 20:23:06 +0000

    quick fix

--- project/project/urls.py
+++ project/project/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("/", include("blog.urls")),
-    path("auth/", include("auth.urls")),
+    path("auth/", include("registrar.urls")),
 ]
