28930e6aff27c5737c3c8f2c1fd9374e74bb923c
NotDaPoyo <168030791+NotDaPoyo@users.noreply.github.com>
2024-04-25 10:11:04 +0000

    Post model

--- project/blog/models.py
+++ project/blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.conf import settings
+ 
+User = settings.AUTH_USER_MODEL
 
 # Create your models here.
+class post(models.Model):
+    author = models.ForeignKey(User, default=1, null=True, on_delete=models.SET_NULL)
+    title = models.CharField(max_length=200)
+    content = models.CharField(max_length=10000)
