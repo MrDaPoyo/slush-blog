4a7fd62e9f598e6ed868cd42517a0b5fbbb789fc
NotDaPoyo <168030791+NotDaPoyo@users.noreply.github.com>
2024-04-25 11:27:25 +0000

    Save 1

--- project/blog/forms.py
+++ project/blog/forms.py
@@ -1,5 +1,12 @@
 from django import forms
+from .models import post
 
-class postForm(forms.Form):
+class postForm(forms.ModelForm):
     title = forms.CharField(required=True)
     content = forms.CharField(required=True)
+    class Meta:
+        model = post
+        fields = [
+            'title',
+            'content'
+        ]
--- project/blog/views.py
+++ project/blog/views.py
@@ -5,8 +5,16 @@ from .forms import postForm
 
 # Create your views here.
 def home(request):
+    form = postForm
     context = {
         "posts" : post.objects.all(),
-        "form": postForm
+        "form": form
     }
+    if request.method == "POST":
+        if form.is_valid :
+            form = postForm(request.POST)
+            form.user = request.user
+            form.title = request.POST.get('title')
+            form.content = request.POST.get('content')
+            form.save()
     return render(request, "index.html", context)
