e4a33c958d0e677450c6e00c503621ab3fe0d90e
NotDaPoyo <168030791+NotDaPoyo@users.noreply.github.com>
2024-04-26 08:08:03 +0000

    remove unused import

--- project/blog/urls.py
+++ project/blog/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
-
+ 
 urlpatterns = [
     path('home/<int:id>', views.home, name="home"),
     path('home/', views.homeRedirect),
