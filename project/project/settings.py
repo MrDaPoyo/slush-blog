4deb32f6da1c053e6e30f4bb5247443a648b83b5
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2024-05-08 21:58:10 +0200

    Update settings.py

--- project/project/settings.py
+++ project/project/settings.py
@@ -122,6 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
